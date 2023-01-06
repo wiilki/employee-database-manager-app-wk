22d40da527b9682e6aa0ea2451fbbdbbebfe8138
Will <willkim0608@gmail.com>
2023-01-06 13:10:09 -0800

    Add basic prompts to index.js. Reinstall package and change Inquirer version

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const consoleTable = require('console.table');
+
+const mainMenu = [
+    {
+        type: 'list',
+        name: 'actions',
+        message: 'What would you like to do?',
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+    }
+];
+
+function init() {
+    return userResponses = inquirer.prompt(mainMenu);
+}
+
+init()
