6efe0e8b4ac46738f319e02ff68b9b4f9e938fbe
Will <willkim0608@gmail.com>
2023-01-06 12:24:16 -0800

    Remove numbers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,20 @@
 INSERT INTO department (name)
 VALUES
- 1   ("Sales"),
- 2   ("Engineering"),
- 3   ("Finance"),
- 4   ("Legal");
+    ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");
 
 INSERT INTO role (title, salary, department_id)
 VALUES
- 1   ("Sales Lead", 100000, 1),
- 2   ("Salesperson", 80000, 1),
- 3   ("Lead Engineer", 150000, 2),
- 4   ("Software Engineer", 120000, 2),
- 5   ("Account Manager", 160000, 3),
- 6   ("Accountant", 125000, 3),
- 7   ("Legal Team Lead", 250000, 4),
- 8   ("Lawyer", 190000, 4)
+    ("Sales Lead", 100000, 1),
+    ("Salesperson", 80000, 1),
+    ("Lead Engineer", 150000, 2),
+    ("Software Engineer", 120000, 2),
+    ("Account Manager", 160000, 3),
+    ("Accountant", 125000, 3),
+    ("Legal Team Lead", 250000, 4),
+    ("Lawyer", 190000, 4)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
