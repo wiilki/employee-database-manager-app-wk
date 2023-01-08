c0e055ef554a0e978be850d3ed4c49307341e6c6
Will <willkim0608@gmail.com>
2023-01-07 21:48:16 -0800

    Edit db.query for employee.js

--- db/query.sql
+++ db/query.sql
@@ -1,2 +1,8 @@
+-- VIEW DEPARTMENTS
+SELECT * FROM department;
+
 -- VIEW ROLES
-ELECT role.id, role.title, department.name AS department, role.salary FROM role JOIN department ON role.department_id = department.id;
+SELECT role.id, role.title, department.name AS department, role.salary FROM role JOIN department ON role.department_id = department.id;
+
+-- VIEW EMPLOYEES
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id AS manager FROM employee INNER JOIN role ON employee.id = role.id INNER JOIN department ON role.department_id = department.id;
