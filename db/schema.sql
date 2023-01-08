4ca4976724d92128aab59ff7ee651e4214217eb2
Will <willkim0608@gmail.com>
2023-01-07 22:13:56 -0800

    Add self referencing foreign key for employee table

--- db/schema.sql
+++ db/schema.sql
@@ -25,7 +25,9 @@ CREATE TABLE employee (
   role_id INT,
   manager_id INT,
   FOREIGN KEY (role_id)
-  REFERENCES role(id)
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
   ON DELETE SET NULL
 );
 
