97a90330a5dad2088a89a3c3db51633994b52cf0
Will <willkim0608@gmail.com>
2023-01-10 12:16:46 -0800

    Add viewByManger function

--- index.js
+++ index.js
@@ -205,8 +205,15 @@ function updateManager() {
                 }
                 start();
             });
-    })
-}
+    });
+};
+
+function viewByManager() {
+    connection.query("SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id WHERE e.manager_id IS NOT NULL ORDER BY e.manager_id;", function (err, results) {
+        console.table(results);
+        start();
+    });
+};
 
 function start() {
     inquirer.prompt([
@@ -214,7 +221,7 @@ function start() {
             type: 'list',
             name: 'actions',
             message: 'What would you like to do?',
-            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'Update Employee Manager', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'Update Employee Manager', , 'View By Manager', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
         }
     ])
         .then(response => {
@@ -227,6 +234,9 @@ function start() {
                     break;
                 case 'Update Employee Role':
                     return updateRole(response);
+                    break;
+                case 'View By Manager':
+                    return viewByManager(response);
                     break;
                 case 'Update Employee Manager':
                     return updateManager(response);
