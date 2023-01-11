7f5d1fc6dea27db7011674043cc0f7722a9a39ec
Will <willkim0608@gmail.com>
2023-01-11 11:00:20 -0800

    Fix syntax error

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function viewByManager() {
 
 // View employee's by departments
 function viewByDepartment() {
-    connection.query("SELECT d.name AS department, e.id, e.first_name, e.last_name, r.title, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id ORDER BY e.manager_id;", function (err, results) {
+    connection.query("SELECT d.name AS department, e.id, e.first_name, e.last_name, r.title, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id ORDER BY r.department_id;", function (err, results) {
         console.log
         console.table(results);
         updateEmployee();
