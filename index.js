ebd6de7497d03d113e87388a9f52ca62ecc29f44
Will <willkim0608@gmail.com>
2023-01-11 15:44:59 -0800

    Add ASC order

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function viewEmployees() {
 
 // View employee's by managers
 function viewByManager() {
-    connection.query("SELECT CONCAT(m.first_name, ' ', m.last_name) AS manager, e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id WHERE e.manager_id IS NOT NULL ORDER BY e.manager_id;", function (err, results) {
+    connection.query("SELECT CONCAT(m.first_name, ' ', m.last_name) AS manager, e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id WHERE e.manager_id IS NOT NULL ORDER BY e.manager_id ASC;", function (err, results) {
         console.table(results);
         updateEmployee();
     });
@@ -46,7 +46,7 @@ function viewByManager() {
 
 // View employee's by departments
 function viewByDepartment() {
-    connection.query("SELECT d.name AS department, e.id, e.first_name, e.last_name, r.title, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id ORDER BY r.department_id;", function (err, results) {
+    connection.query("SELECT d.name AS department, e.id, e.first_name, e.last_name, r.title, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id ORDER BY r.department_id ASC;", function (err, results) {
         console.log
         console.table(results);
         updateEmployee();
